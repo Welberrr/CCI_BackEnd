f9ed4423bd4aaced51fe24f9fa0eb3ae6fbbea19
Welber Henrique <welberhenrique.costa@gmail.com>
2026-08-02 00:07:13 -0300

    Feat: Estruturacao do projeto initializr

---
+++ cafecomingles/src/main/java/com/cci/cafecomingles/CafecominglesApplication.java
@@ -0,0 +1,13 @@
+package com.cci.cafecomingles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafecominglesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafecominglesApplication.class, args);
+	}
+
+}
---
+++ cafecomingles/src/test/java/com/cci/cafecomingles/CafecominglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cci.cafecomingles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafecominglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
